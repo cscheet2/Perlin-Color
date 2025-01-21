1c5a1afecd76b0824f3d0b794f3837c6c2405a66
Raegan Scheet <CamScheet@gmail.com>
2025-01-21 12:53:31 -0600

    refactored color logging to be better and print to stderr

--- colormap.c
+++ colormap.c
@@ -10,38 +10,54 @@
 
 #include "./colormap.h"
 
+static long long unsigned* palette_log = NULL;
+static inline void init_log(const uint8_t log_length);
+static inline void update_log(const uint8_t index);
+static inline void free_log();
+static inline void print_log_index_precentages(const palette_t* palette);
+
 void colorSurface(const char* color_name, SDL_Surface* surface, uint8_t numOctaves, uint16_t width, uint16_t height) {
 	reshufflePermutationArray();
-	long long unsigned l0 = 0, l1 = 0, l2 = 0, l3 = 0, l4 = 0, l5 = 0, l6 = 0, l7 = 0, l8 = 0, l9 = 0;
   uint32_t* pixels = (uint32_t*)surface->pixels;
 	palette_t* palette = load_color_palette(color_name);
-	float scale = 0.01F;
+  init_log(palette->num_colors);
+	const float scale = 0.01F;
   for (uint16_t y = 0; y < surface->h; y++) {
 		for (uint16_t x = 0; x < surface->w; x++) {
 			float noise = ((fractalBrownianMotion2D((float)x * scale, (float)y * scale, numOctaves)) + 1.0F) / 2.0F;
 			uint8_t index = (uint8_t)(noise * (palette->num_colors));
-			pixels[y * surface->w + x] = SDL_MapRGBA(surface->format, palette->colors[index].r, palette->colors[index].g, palette->colors[index].b, 255);	     
-			     if (index == 0) { l0++; }
-			else if (index == 1) { l1++; }
-			else if (index == 2) { l2++; }
-			else if (index == 3) { l3++; }
-			else if (index == 4) { l4++; }
-			else if (index == 5) { l5++; }
-			else if (index == 6) { l6++; }
-			else if (index == 7) { l7++; }
-			else if (index == 8) { l8++; }
-			else if (index == 9) { l9++; }
-			// printf("%3d: (%3d,%3d) <- (%3d, %3d, %3d), %f\n", (int)index, (int)x, (int)y, (int)palette->colors[index].r&0xFF, (int)palette->colors[index].g&0xFF, (int)palette->colors[index].b&0xFF, noise);
+			pixels[y * surface->w + x] = SDL_MapRGBA(surface->format, palette->colors[index].r, palette->colors[index].g, palette->colors[index].b, 255);
+      update_log(index);
 		}
 	}
-	long double sum = l0 + l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9;
-	long double a0 = ((long double)l0/sum)*100, a1 = ((long double)l1/sum)*100, a2 = ((long double)l2/sum)*100, a3 = ((long double)l3/sum)*100, a4 = ((long double)l4/sum)*100;
-	long double a5 = ((long double)l5/sum)*100, a6 = ((long double)l6/sum)*100, a7 = ((long double)l7/sum)*100, a8 = ((long double)l8/sum)*100, a9 = ((long double)l9/sum)*100;
-	printf("Total: %llu\n", (long long unsigned)sum);
-	printf("l0 = %5llu, %3.02Lf%%\nl1 = %5llu, %3.02Lf%%\nl2 = %5llu, %3.02Lf%%\nl3 = %5llu, %3.02Lf%%\nl4 = %5llu, %3.02Lf%%\n", 
-					l0, a0, l1, a1, l2, a2, l3, a3, l4, a4);
-	printf("l5 = %5llu, %3.02Lf%%\nl6 = %5llu, %3.02Lf%%\nl7 = %5llu, %3.02Lf%%\nl8 = %5llu, %3.02Lf%%\nl9 = %5llu, %3.02Lf%%\n", 
-					l5, a5, l6, a6, l7, a7, l8, a8, l9, a9);
+  print_log_index_precentages(palette);
   free_palette(palette);
+  free_log();
 }
 
+static inline void init_log(const uint8_t log_length) {
+  palette_log = (long long unsigned*) calloc(log_length, sizeof(long long unsigned));
+}
+static inline void update_log(const uint8_t index) {
+  palette_log[index] += 1;
+}
+static inline void free_log() {
+  free(palette_log);
+}
+static inline void print_log_index_precentages(const palette_t* palette) {
+  long double sum = 0;
+  for (uint8_t index = 0; index < palette->num_colors; index++) { sum += palette_log[index]; }
+
+  fprintf(stderr, "|-------|--------|---------|-----------------|\n");
+  fprintf(stderr, "| index | amount | precent | RGB color value |\n");
+  fprintf(stderr, "|-------|--------|---------|-----------------|\n");
+  
+  for (uint8_t index = 0; index < palette->num_colors; index++) {
+    fprintf(stderr, "| %5d ", (int)index);
+    fprintf(stderr, "| %6llu ", palette_log[index]);
+    fprintf(stderr, "| %6.02Lf%% ", ((long double)palette_log[index] / sum) * 100);
+    fprintf(stderr, "| (%3u, %3u, %3u) ", (unsigned int) palette->colors[index].r, (unsigned int) palette->colors[index].g, (unsigned int) palette->colors[index].b);
+    fprintf(stderr, "|\n");
+  }
+  fprintf(stderr, "|-------|--------|---------|-----------------|\n");
+}
